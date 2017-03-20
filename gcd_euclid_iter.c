406b673429c6ba34a715240841e8be2b3ffae475
Sergey Polyakov <bgtr86@mail.ru>
2017-03-20 14:57:09 +0300

    НОД
    Итеративный алгоритм Евклида для поиска НОД двух целых чисел.

---
+++ gcd_euclid_iter.c
@@ -0,0 +1,32 @@
+/*
+ *
+ * Вычисление наибольшего общего делителя (НОД)
+ * двух целых чисел.
+ *
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int gcd(int a, int b);
+
+ int main() {
+    int a, b;
+
+    scanf("%d%d", &a, &b);
+    printf("GCD a and b: %d\n", gcd(a, b));
+
+    system("PAUSE");
+
+    return 0;
+ }
+
+ int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+ }
